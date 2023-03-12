01ebdb41140c68e28539f631a28e972e769ac5be
mihai185 <bochi_85@yahoo.com>
2023-03-12 15:13:42 +0100

    aa

---
+++ tema liste.py
@@ -0,0 +1,21 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+my_list.reverse()
+print(my_list)
+
+my_list.sort()
+my_sliced_list = my_list[1::2]
+print(my_sliced_list)
+
+my_list.sort()
+my_sliced_list = my_list[::2]
+print(my_sliced_list)
+
+my_list.sort()
+my_sliced_list = my_list[2:9:3]
+print(my_sliced_list)
+
